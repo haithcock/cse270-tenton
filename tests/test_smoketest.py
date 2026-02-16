d6f36e3c42a058cad46d340cfe106387d8f7d975
haithcock <dakota.haith1997@gmail.com>
2026-02-16 00:03:21 -0500

    added headless thingy

--- tests/test_smoketest.py
+++ tests/test_smoketest.py
@@ -10,6 +10,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.firefox.options import Options
+
 
 class TestSmoketest():
   def setup_method(self, method):
