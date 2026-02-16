a486f3fa8e1209d221bbaad6bb7ddece2a4edbcb
haithcock <dakota.haith1997@gmail.com>
2026-02-16 01:45:51 -0500

    makin moves

--- tests/test_smoketest.py
+++ tests/test_smoketest.py
@@ -68,11 +68,10 @@ class TestSmoketest():
     self.driver.find_element(By.NAME, "fname").click()
     elements = self.driver.find_elements(By.NAME, "fname")
     assert len(elements) > 0
-    self.vars["John"] = self.driver.execute_script('document.querySelector("[name=\\"fname\\"]").value = "John";')
-    self.vars["Doe"] = self.driver.execute_script('document.querySelector("[name=\\"lname\\"]").value = "Doe";')
-    self.vars["No_thanks_LLC"] = self.driver.execute_script('document.querySelector("[name=\\"bizname\\"]").value = "No_thanks_LLC";')
-    self.vars["Duke"] = self.driver.execute_script('document.querySelector("[name=\\"biztitle\\"]").value = "Duke";')
-
+    self.vars["John"] = self.driver.execute_script("document.querySelector(\'[name=\'fname\']\').value = \'John\';")
+    self.vars["Doe"] = self.driver.execute_script("document.querySelector(\'[name=\'lname\']\').value = \'Doe\';")
+    self.vars["No_thanks_LLC"] = self.driver.execute_script("document.querySelector(\'[name=\'bizname\']\').value = \'No_thanks_LLC\';")
+    self.vars["Duke"] = self.driver.execute_script("document.querySelector(\'[name=\'biztitle\']\').value = \'Duke\';")
     self.driver.find_element(By.NAME, "submit").click()
     elements = self.driver.find_elements(By.NAME, "email")
     assert len(elements) > 0
@@ -81,9 +80,8 @@ class TestSmoketest():
     self.driver.get("http://127.0.0.1:5500/cse270-tenton/teton/1.6/admin.html")
     self.driver.set_window_size(1690, 1105)
     self.driver.find_element(By.ID, "username").click()
-    self.vars["George"] = self.driver.execute_script('document.querySelector("[name=\\"username\\"]").value = "Geogre";')
-    self.vars["Washington"] = self.driver.execute_script('document.querySelector("[name=\\"password\\"]").value = "Washington";')
-
+    self.vars["George"] = self.driver.execute_script("document.querySelector(\'[name=\'username\']\').value = \'Geogre\';")
+    self.vars["Washington"] = self.driver.execute_script("document.querySelector(\'[name=\'password\']\').value = \'Washington\';")
     self.driver.find_element(By.CSS_SELECTOR, ".mysubmit:nth-child(4)").click()
     elements = self.driver.find_elements(By.ID, "username")
     assert len(elements) > 0
