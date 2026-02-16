e5cb6ec7d940728b22718d3bb407130892bcced7
haithcock <dakota.haith1997@gmail.com>
2026-02-16 15:10:12 -0500

    added firefox webdriver thing

--- tests/test_smoketest.py
+++ tests/test_smoketest.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.Firefox.options import Options
 
 class TestSmoketest():
   def setup_method(self, method):
