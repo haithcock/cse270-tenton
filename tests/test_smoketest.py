b8ee038b21b65e82810924dc0420cc6c2b9aed65
haithcock <dakota.haith1997@gmail.com>
2026-02-16 14:57:27 -0500

    updated pytest

--- tests/test_smoketest.py
+++ tests/test_smoketest.py
@@ -9,9 +9,13 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+
 
 class TestSmoketest():
   def setup_method(self, method):
+    options = Options()
+    options.add_argument("--headless=new")
     self.driver = webdriver.Firefox()
     self.vars = {}
   
